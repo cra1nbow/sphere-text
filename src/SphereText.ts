987d49cfdfb41946fe84111d17ea038575cd22c7
cra1nbow <dev@cra1nbow.is>
2021-10-26 19:03:27 +0900

    feat: init three.js

--- src/SphereText.ts
+++ src/SphereText.ts
@@ -1,7 +1,34 @@
 import { LitElement, html } from 'lit';
+import { property } from 'lit/decorators.js';
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
 export class SphereText extends LitElement {
+  @property({ type: String }) text = 'love';
+
+  scene = new Scene();
+
+  camera = new PerspectiveCamera();
+
+  renderer = new WebGLRenderer();
+
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('resize', this._onChangeSize);
+    this._onChangeSize();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener('resize', this._onChangeSize);
+  }
+
+  _onChangeSize = () => {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+  };
+
   render() {
-    return html` <main></main> `;
+    return html`<main>${this.renderer.domElement}</main>`;
   }
 }
